6f06f7077dccfd02dc0bf029afe4f776e6699cce
Robert Kalescky <rkalescky@smu.edu>
2022-08-10 10:19:28 -0500

    C++ added string type.

--- examples/cpp/00_types_expressions_statements.cpp
+++ examples/cpp/00_types_expressions_statements.cpp
@@ -14,8 +14,9 @@
  * https://en.cppreference.com/
 */
 
-// Include statement
+// Include statements
 #include <iostream>
+#include <string>
 
 // Namespace declaration statement
 using namespace std;
@@ -34,24 +35,25 @@ int main() {
     // of the syntax. Below is a table of variable types with the usual the sizes
     // and value ranges noted for 64-bit Unix using the LP64 data model. Expressions
     // are used to denote the relationship between variables.
-    // Type       Variable Name   Initial Value   Usual Size on
-    short int                si = -1;             // 16-bits, [-2^15, 2^15-1]
-    unsigned short int      usi =  1;             // 16-bits, [0, 2^16-1]
-    int                       i = -10;            // 32-bits, [-2^31, 2^31-1]
-    unsigned int             ui =  10;            // 32-bits, [0, 2^32-1]
-    long int                 li = -1234567890;    // 64-bits, [-2^63,-2^63-1]
-    unsigned long int       uli =  1234567890;    // 64-bits, [0,-2^64-1]
-    long long int           lli = -1234567890;    // 64-bits, [-2^63,-2^63-1]
-    unsigned long long int ulli =  1234567890;    // 64-bits, [0,-2^64-1]
-    bool                      b =  true;          // 1-bit, true or false, [1, 0]
-    signed char              sc =  'a';           // 8-bits, [-127, 127]
-    unsigned char            uc =  'b';           // 8-bits, [0, 255]
-    char                      c =  'c';           // 8-bits, [-127, 127] (x86_64) or [0, 255] (ARM)
-    float                     f =  3.14159;       // 32-bits, signed decimal number
-    double                    d =  3.14159;       // 64-bits, signed decimal number
-    long double              ld =  3.14159;       // 80-bits (x86_64) or 128-bits (ARM)
-    auto                      a =  d + i;         // Automatic typing based on initialization (a is double)
-    const int                ci =  42;            // Immutable variable
+    // Type       Variable Name   Initial Value     Usual Size on
+    short int                si = -1;               // 16-bits, [-2^15, 2^15-1]
+    unsigned short int      usi =  1;               // 16-bits, [0, 2^16-1]
+    int                       i = -10;              // 32-bits, [-2^31, 2^31-1]
+    unsigned int             ui =  10;              // 32-bits, [0, 2^32-1]
+    long int                 li = -1234567890;      // 64-bits, [-2^63,-2^63-1]
+    unsigned long int       uli =  1234567890;      // 64-bits, [0,-2^64-1]
+    long long int           lli = -1234567890;      // 64-bits, [-2^63,-2^63-1]
+    unsigned long long int ulli =  1234567890;      // 64-bits, [0,-2^64-1]
+    bool                      b =  true;            // 1-bit, true or false, [1, 0]
+    signed char              sc =  'a';             // 8-bits, [-127, 127]
+    unsigned char            uc =  'b';             // 8-bits, [0, 255]
+    char                      c =  'c';             // 8-bits, [-127, 127] (x86_64) or [0, 255] (ARM)
+    float                     f =  3.14159;         // 32-bits, signed decimal number
+    double                    d =  3.14159;         // 64-bits, signed decimal number
+    long double              ld =  3.14159;         // 80-bits (x86_64) or 128-bits (ARM)
+    auto                      a =  d + i;           // Automatic typing based on initialization (a is double)
+    const int                ci =  42;              // Immutable variable
+    string                    s =  "Hello, World!"; // Strings are collections of characters
     
     // Literals can be used to define types inline
     int                 il = i   + 2;    // 2 is signed int
