c338fe80a7fd9c4c8152727e85cd63a140d206df
Robert Kalescky <rkalescky@smu.edu>
2022-08-17 17:57:09 -0500

    C++ operators and conditionals.

---
+++ examples/cpp/01_operators_conditionals.cpp
@@ -0,0 +1,92 @@
+/**
+ \brief Overview of C++ Operators and Conditionals
+ 
+ 
+ */
+
+// Include statement
+#include <iostream>
+#include <string>
+#include <bitset> // bitset
+#include <cmath>  // fmod, remainder
+
+// Namespace declaration statement
+using namespace std;
+
+// Program entry-point, encapsulated by the curly brackets { and }
+int main() {
+
+    // Declare initialized integer variable
+    int n = 42;
+    int m = -2;
+    
+    // Arithmetic Operators
+
+    // Postfix operators return the original value
+    cout << n++ << endl; // Post-increment, n is now 43, but 42 is printed
+    cout << n-- << endl; // Post-decrement, n is now 42, but 43 is printed
+
+    // Prefix operators return the modified value
+    cout << ++n << endl; // Pre-increment, n is now 43 and 43 is printed
+    cout << --n << endl; // Pre-decrement, n is now 42 and 42 is printed
+    
+    // Unary operators
+    cout << +n << endl; // Unary plus, positive 42 is printed
+    cout << -n << endl; // Unary minus, negative -42 is printed
+    
+    // Binary operators
+    cout << n * m << endl;           // Multiplication, -84 is printed
+    cout << n / m << endl;           // Division, -21 is printed
+    cout << n % m << endl;           // Modulo (integer only), 0 is printed
+    cout << fmod(n, m) << endl;      // Modulo (floats, truncated), 0.0 is printed
+    cout << remainder(n, m) << endl; // Modulo (floats, rounded), 0.0 is printed
+    cout << n + m << endl;           // Addition, 40 is printed
+    cout << n - m << endl;           // Subtraction, 44 is printed
+    
+    // Boolean operators
+    
+    bool a = true;  // Has value of 1
+    bool b = false; // has value of 0
+
+    // Unary operators
+    cout << !a << endl; // Not, 0 (false) is printed
+    
+    // Binary operators
+    cout << (n >  m) << endl; // Greater than, 1 (true) is printed
+    cout << (n >= m) << endl; // Greater than or equal to, 1 (true) is printed
+    cout << (n <  m) << endl; // Less than, 0 (false) is printed
+    cout << (n <= m) << endl; // Less than or equal to, 0 (false) is printed
+    cout << (n == m) << endl; // Equal to, 0 (false) is printed
+    cout << (n != m) << endl; // Not equal to, 1 (true) is printed
+    cout << (a && b) << endl; // Logical AND, 0 (false) is printed
+    cout << (a || b) << endl; // Logical OR, 1 (true) is printed
+
+    // Bitwise operators
+    
+    bitset<3> i{"100"}; // 3-bit binary representation of 4
+    bitset<3> j{"010"}; // 3-bit binary representation of 2
+
+    cout << ~i       << endl; // One's complement (flips each bit), 011 is printed
+    cout << (i &  j) << endl; // AND, 000
+    cout << (i ^  j) << endl; // Exclusive OR
+    cout << (i |  j) << endl; // Inclusive OR
+    cout << (i << 1) << endl; // Left shift, 000 is printed
+    cout << (i >> 1) << endl; // Right shift, 010 is printed
+
+    // Assignment operators
+
+    int x = 3;
+    int y = 2;
+
+    x   = y; cout << "Assignment, x is now "                                  << x << endl;
+    x  *= y; cout << "Multiplication assignment, x = x * y, x is now "        << x << endl;
+    x  /= y; cout << "Division assignment, x = x / y, x is now "              << x << endl;
+    x  %= y; cout << "Modulo assignment (integer only), x = x % y, x is now " << x << endl;
+    x  += y; cout << "Addition assignment, x = x + y, x is now "              << x << endl;
+    x  -= y; cout << "Subtraction assignment, x = x - y, x is now "           << x << endl;
+    x <<= 1; cout << "Bitwise shift left assignment, x = x << y, x in now "   << x << endl;
+    x >>= 1; cout << "Bitwise shift right assignment, x = x << y, x in now "  << x << endl; 
+    
+    return 0;
+}
+
