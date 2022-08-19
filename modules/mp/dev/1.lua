62b09a919cc3c1e495646931e06a9057ab2fec64
Robert Kalescky <rkalescky@smu.edu>
2022-08-19 15:48:57 -0500

    Added neovim to MP dev module.

--- modules/mp/dev/1.lua
+++ modules/mp/dev/1.lua
@@ -1,5 +1,6 @@
 prepend_path("MODULEPATH", "/hpc/mp/spack/share/spack/modules/linux-ubuntu20.04-zen2")
-load("git-2.35.2-gcc-9.4.0-yqma3wo")
-load("git-lfs-3.1.2-gcc-9.4.0-hmsglah")
-load("cmake-3.23.1-gcc-10.3.0-faucxp3")
-load("dos2unix-7.4.2-gcc-10.3.0-hvhqlq4")
+load("git-2.35.2-gcc-9.4.0-yqma3wo",
+     "git-lfs-3.1.2-gcc-9.4.0-hmsglah",
+     "cmake-3.23.1-gcc-10.3.0-faucxp3",
+     "dos2unix-7.4.2-gcc-10.3.0-hvhqlq4",
+     "neovim-0.7.0-gcc-10.3.0-terkir3")
