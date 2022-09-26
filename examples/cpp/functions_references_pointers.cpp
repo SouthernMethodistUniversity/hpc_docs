9f2f98c35448e0199e0cce375c57a59e857751c3
John LaGrone <jlagrone@gmail.com>
2022-09-26 11:39:06 -0500

    Add example of functors and lambda

--- examples/cpp/functions_references_pointers.cpp
+++ examples/cpp/functions_references_pointers.cpp
@@ -7,9 +7,9 @@
 */
 
 #include <iostream> // standard IO
-#include <vector> // standard "vector" library for dynamic arrays
-#include <array> // standard "array" library for static arrays
-#include <memory> // For different pointer types, e.g. smart pointers
+#include <vector>   // standard "vector" library for dynamic arrays
+#include <array>    // standard "array" library for static arrays
+#include <memory>   // For different pointer types, e.g. smart pointers
 
 /*
 
@@ -50,15 +50,15 @@
 // outside the function, changes to the memory the variable points to
 // will persist after the function is complete
 //
-// Which one should you chose? It depends. For things that are "small" 
+// Which one should you chose? It depends. For things that are "small"
 // like ints, doubles, etc. It doesn't matter too much.
 // For large data objects (like a large array), copying the entire array
-// may require significantly more resources (time, memory, etc.) So a 
+// may require significantly more resources (time, memory, etc.) So a
 // reference or pointer is a better choice.
 //
 // As an aside. Pointers are the "old" C-style why of doing things. In
 // most cases it is better to use a reference. The code is clearer and
-// it usually also better for memory management reasons. If you need 
+// it usually also better for memory management reasons. If you need
 // to use pointers, it is usually better to use a smart pointer,
 // shared pointer, unique pointer, etc than the C-style pointer.
 
@@ -74,14 +74,14 @@ void increment_by_value(int a)
 };
 
 // Pass value by reference instead
-void increment_by_reference(int& a)
+void increment_by_reference(int &a)
 {
   ++a; // this is the same as the input
   std::cout << "a in function is: " << a << std::endl;
 };
 
 // pass by points
-void increment_by_pointer(int* a)
+void increment_by_pointer(int *a)
 {
   ++(*a); // this is the same memory location as input
   std::cout << "a in function is: " << *a << std::endl;
@@ -91,11 +91,12 @@ void increment_by_pointer(int* a)
 // or constant (meaning they cannot be changed by the function)
 // Pass by value is automatically constant because the function operates
 // on a copy and can't modify the input.
-double mutable_compute_norm_squared(std::vector<double>& v)
+double mutable_compute_norm_squared(std::vector<double> &v)
 {
   double solution = 0;
   // loop over each element in the vector
-  for (const auto& element : v) {
+  for (const auto &element : v)
+  {
     solution += element * element;
   }
 
@@ -117,23 +118,24 @@ double const_compute_norm_squared(const std::vector<double> &v)
 
   // this would cause a compiler error because we
   // cannot change v
-  //v.push_back(solution);
+  // v.push_back(solution);
 
   // return the result
   return solution;
 }
 
-// We can add default arguments functions. 
+// We can add default arguments functions.
 // Any inputs with a default must be listed last
 // This will optionally print the answer from inside the function,
 // defaulting to false
-double compute_area(const double& radius, bool print = false)
+double compute_area(const double &radius, bool print = false)
 {
   // NOTE: M_PI is defined in most compilers, but not all so use with care
   const double pi = 3.1415926535897932384;
   double area = pi * radius * radius;
 
-  if (print) {
+  if (print)
+  {
     std::cout << "Inside function, area = " << area << std::endl;
   }
 
@@ -141,33 +143,33 @@ double compute_area(const double& radius, bool print = false)
 }
 
 // Overloading.
-// 
-// C++ allows you to have multiple functions with the same name 
+//
+// C++ allows you to have multiple functions with the same name
 // as long as the argument list is different.
-double add_numbers(int a, int b) 
+double add_numbers(int a, int b)
 {
   std::cout << "example 1 \n";
-  return a+b;
+  return a + b;
 }
-double add_numbers(double a, double b) 
+double add_numbers(double a, double b)
 {
   std::cout << "example 2 \n";
-  return a+b;
+  return a + b;
 }
 double add_numbers(double a, float b, int c)
 {
   std::cout << "example 3 \n";
-  return a+b+c;
+  return a + b + c;
 }
 double add_numbers(int a, double b, float c)
 {
   std::cout << "example 4 \n";
-  return a+b+c;
+  return a + b + c;
 }
 
 // changing the return type, but not the arguments
 // will cause errors
-//int add_numbers(int a, double b, float c)
+// int add_numbers(int a, double b, float c)
 //{
 //  return a + b + c;
 //}
@@ -176,20 +178,21 @@ double add_numbers(int a, double b, float c)
 // or pass by reference, etc. will cause errors because they
 // compiler won't be able to tell which on you wanted.
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
   // The main function is special. argv contains a list of command line arguments
   // and argc is the number of arguments. On most systems, there is always 1 argument,
   // the function name
   // print out arguments
-  for (int i=0; i<argc; ++i) {
+  for (int i = 0; i < argc; ++i)
+  {
     std::cout << "argument " << i << " = " << argv[i] << std::endl;
   }
   std::cout << "/n/n";
 
   int i = 4;
-  
+
   std::cout << "i = " << i << std::endl;
 
   std::cout << "\nincrement by value" << std::endl;
@@ -208,22 +211,24 @@ int main(int argc, char* argv[])
   // increment_by_reference(i + 7);
 
   // Create a vector containing doubles
-  std::vector<double> v = {7, 5, 16, 8}; // list initialized requires c++11
+  std::vector<double> v = {7, 5, 16, 8}; // list initialized requires c++11 or later
 
   // compute norm squared
   std::cout << "norm squared: " << mutable_compute_norm_squared(v) << std::endl;
   std::cout << "norm squared: " << const_compute_norm_squared(v) << std::endl;
 
   // computing the area of a circle
-  std::cout << "\nArea of a circle (r = 1) with default args: \n" << compute_area(1.0) << std::endl;
-  std::cout << "Area of a circle (r = 1) with print = true: \n" << compute_area(1.0, true) << std::endl;
+  std::cout << "\nArea of a circle (r = 1) with default args: \n"
+            << compute_area(1.0) << std::endl;
+  std::cout << "Area of a circle (r = 1) with print = true: \n"
+            << compute_area(1.0, true) << std::endl;
 
   // call overloaded functions
   int a = 2;
   float b = 3;
   double c = 9;
 
-  std::cout << "\nadd_numbers(int, int):  (expect example 1) " << add_numbers(a,a) << std::endl;
+  std::cout << "\nadd_numbers(int, int):  (expect example 1) " << add_numbers(a, a) << std::endl;
   std::cout << "add_numbers(double, double):  (expect example 2) " << add_numbers(c, c) << std::endl;
   std::cout << "add_numbers(double, float, int):  (expect example 3) " << add_numbers(c, b, a) << std::endl;
   std::cout << "add_numbers(int, double, float):  (expect example 4) " << add_numbers(a, c, b) << std::endl;
---
+++ examples/cpp/functors_and_lamda_functions.cpp
@@ -0,0 +1,151 @@
+/*
+ Functors and Lambda Functions
+
+ Useful References:
+ * Discovering Modern C++ by Peter Gottschling (amongst many other C++ books)
+ * https://en.cppreference.com/
+*/
+
+#include <iostream> // standard IO
+#include <vector>   // standard "vector" library for dynamic arrays
+#include <algorithm> // for the transform functions
+
+/*
+
+A functor is essentially a class that has defined the "()" operator.
+
+This lets us define objects that behave like functions.
+
+There are 2 main reasons we might want a function like object:
+
+  1.  function like object allow us to define, use, and modify
+      properties, for instance the current state of an object.
+      In some applications this may be valueable and simplify 
+      the code (e.g. the function "knows" informatin that you
+      would otherwise have to pass to the function.) This also
+      allows you to initialize frequently used values once, 
+      which is especially useful if they're expensive to compute.   
+
+  2.  By using a functor, the compiler is often able to do a 
+      better job optimizing. Essentially this happens because
+      the compiler knows exactly what function is being called,
+      whereas if you pass a pointer to a function, the compiler
+      just knows a function is being called (but not necessarily
+      which one.) This means, for example, the compiler might
+      inline a functor.
+
+*/
+
+
+// example of a simple functor 
+// You see functors defined as structs instead of classes, classes are
+// more general structure with extra features (that we're not using)
+class increment
+{
+private:
+    // define a private member variable to hold the amount we will
+    // increment by
+    // It is common practice to prepend member variables with "m_"
+    // but this is purely a style choice.
+    int m_increment_amount;
+
+public:
+
+    // constructor. This sets the default values.
+    // This needs to be the same name as the class
+    // Note we can overload the constructor. Here,
+    // we're setting the increment amount to 1 if we're
+    // not provided an argument or we're setting it
+    // to the inputed value.
+    increment() : m_increment_amount(1){};
+    increment(const int &n) : m_increment_amount(n){};
+
+    // Now we're going to define the "()" operator 
+    // so we can increment the input and return the
+    // new value
+    int operator()(const int& input_num) const
+    {
+        // add the increment amount to the inputed value
+        // and return that new value
+        return m_increment_amount + input_num;
+    }
+}; // end of increment class. Note, ends with ;
+
+
+// function to print vectors
+void print_vector(const std::vector<int>& v) 
+{
+    for (const auto& val: v) {
+        std::cout << val << ", ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main() {
+
+    // create an array of values to increment
+    std::vector<int> v = {1, 2, 3, 4, 5}; // list initialized requires c++11 or later
+
+    // use our increment functor to add 1
+    increment inc_by_one; // initialize the functor with the default 
+    for (auto &val : v) {
+        val = inc_by_one(val);
+    }
+    print_vector(v);
+
+    // use our increment functor to subtract 1
+    increment dec_by_one(-1); // initialize the functor to subtract one
+    for (auto &val : v)
+    {
+        val = inc_by_one(val);
+    }
+    print_vector(v);
+
+    // use our increment functor to add 2 to each value using the
+    // std::transform function
+    //
+    // There is a subtlety here. The increment(2) argument here is
+    // calling the constructor and setting the increment value. 
+    // Inside the transform function, it is calling v[i] = incremnt(v[i])
+    std::transform(v.begin(), v.end(), v.begin(), increment(2));
+    print_vector(v);
+
+    // subtract 2 from each entry
+    std::transform(v.begin(), v.end(), v.begin(), increment(-2));
+    print_vector(v);
+
+    // The functor we defined above is a lot of work for something that
+    // is pretty trivial (and might be even more work if the function
+    // object needed to be more complex).
+    //
+    // Since C++11, C++ has "lambda expressions," which is basically
+    // a way for the compiler to generate a functor for you using some
+    // shorthand notation
+
+    // Make a lambda function that adds 1
+    // auto requires c++14
+    // the [&] specifies we should capture values by reference
+    // we could also capture by value, pointer, etc.
+    // Then the inputs are in (...), we're taking an int
+    // Finally, we define the function in {...} (just returning input + 1)
+    auto add_one_lambda = [&](const int& val){return val + 1;};
+    std::transform(v.begin(), v.end(), v.begin(), add_one_lambda);
+    print_vector(v);
+
+    // subtract 1 using lambdas
+    auto dec_one_lambda = [&](const int &val){ return val - 1; };
+    std::transform(v.begin(), v.end(), v.begin(), dec_one_lambda);
+    print_vector(v);
+
+    // We can also use lambda functions inline. Here, we'll add 2
+    // Again, for style preferences, I find it easier to read lambda expressions
+    // if they're defined like the above. This example is inline and extactly the
+    // same, but more complicated expressions may be difficult to read.
+    std::transform(v.begin(), v.end(), v.begin(), [&](const int &val){ return val + 2; });
+    print_vector(v);
+
+    // and subtract 3
+    std::transform(v.begin(), v.end(), v.begin(), [&](const int &val){ return val - 3; });
+    print_vector(v);
+}
