5332aa679845cd68ccfc02f48e6659e72b4005b4
Robert Kalescky <rkalescky@smu.edu>
2023-04-19 14:27:09 -0500

    Setup script works without pip.

--- docs/examples/jupyter/setup_env.sh
+++ docs/examples/jupyter/setup_env.sh
@@ -4,8 +4,8 @@ env_dir="$WORK/.venv/jupyter"
 
 python3 -m venv $env_dir
 source $env_dir/bin/activate
-pip3 install -U pip wheel setuptools
-pip3 install -r requirements.txt
+python3 -m pip install -U pip wheel setuptools
+python3 -m pip install -r requirements.txt
 
 echo "Finished installing Python environment."
 echo "Use via:"
