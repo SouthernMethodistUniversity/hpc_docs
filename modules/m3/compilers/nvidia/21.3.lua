7e2b592b675335930a0f9e9ee46ffb7ad8341df0
Robert Kalescky <rkalescky@smu.edu>
2023-04-20 09:40:42 -0500

    Fixed M3 NVIDIA compiler whatis typo.

--- modules/m3/compilers/nvidia/21.3.lua
+++ modules/m3/compilers/nvidia/21.3.lua
@@ -1,4 +1,4 @@
-whatis("Provides access to NVIDIA HPC-SDK 23.1 compiler suite, libraries, and applications.")
+whatis("Provides access to NVIDIA HPC-SDK 21.3 compiler suite, libraries, and applications.")
 family("compiler")
 
 append_path("MODULEPATH", "/hpc/m3/apps/nvidia/hpc-sdk/21.3/modulefiles")
