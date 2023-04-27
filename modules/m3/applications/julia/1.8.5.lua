564f9f519b3d893c550e098fcdb91320ff06a2c5
Robert Kalescky <rkalescky@smu.edu>
2023-04-26 20:24:13 -0500

    Added M3 Julia module.

---
+++ modules/m3/applications/julia/1.8.5.lua
@@ -0,0 +1,8 @@
+whatis("loads Julia 1.7.3 environment")
+family("julia")
+conflict("julia")
+local root = "/hpc/m3/apps/julia/1.8.5"
+prepend_path("PATH", pathJoin(root, "bin"))
+prepend_path("MANPATH", pathJoin(root, "share/man"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib"))
+
