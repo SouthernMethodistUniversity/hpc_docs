fb1d72509c3e8262a8f165f612b929bc225f5a9c
John LaGrone <jlagrone@gmail.com>
2023-04-27 14:33:12 -0500

    add gurobi module

---
+++ modules/m3/applications/gurobi/10.0.1.lua
@@ -0,0 +1,10 @@
+whatis("Gurobi 10.0.1")
+family("gurobi")
+conflict("gurobi")
+
+append_path("PATH", "/hpc/m3/apps/gurobi/10.0.1/linux64/bin")
+setenv("GUROBI_HOME", "/hpc/m3/apps/gurobi/10.0.1/linux64")
+setenv("GRB_LICENSE_FILE", "/hpc/m3/apps/gurobi/10.0.1/linux64/client.lic")
+append_path("LD_LIBRARY_PATH","/hpc/m3/apps/gurobi/10.0.1/linux64/lib")
+append_path("CPATH", "/hpc/m3/apps/gurobi/10.0.1/linux64/include")
+append_path("LIBRARY_PATH","/hpc/m3/apps/gurobi/10.0.1/linux64/lib")
