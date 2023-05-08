1a6adabe2834583d415c768e7c209f75ed424604
Robert Kalescky <rkalescky@smu.edu>
2023-05-08 14:25:56 -0500

    Added M3 deMon module.

---
+++ modules/m3/applications/demon/6.2.2.lua
@@ -0,0 +1,14 @@
+whatis("deMon 6.2.2")
+
+family("demon")
+conflict("demon")
+
+load("gcc/11.2.0", "openblas/0.3.21-iofurbe", "openmpi/4.1.4-7al4h2x")
+
+local root = "/hpc/m3/apps/demon/6.2.2"
+local base = pathJoin(root, "deMon")
+
+setenv("CREX_ROOT", root)
+prepend_path("MANDPATH", pathJoin(base, "man"))
+append_path("PATH", pathJoin(base, "bin"))
+
