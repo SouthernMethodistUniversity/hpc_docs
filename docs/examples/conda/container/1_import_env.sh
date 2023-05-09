16cf1557ab7056e25547edbc7186bd32eee2b799
Robert Kalescky <rkalescky@smu.edu>
2023-05-09 08:14:03 -0500

    Containerized conda environments.
    - Scripts show entire process from initial environment building to exporting to apptainer.
    - micromamba base container is used.
    - Lmod module is made for ease of using the apptainer image.

--- docs/examples/conda/container/1_import_env.sh
+++ docs/examples/conda/container/1_import_env.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Create new environment from file
-mamba env create -n ds -f env.yaml
+mamba env create -n ds -f environment.yaml
 
 # Note that different OS's will have slightly different package sets. The goal
 # should be to have one environment file that runs on all supported platforms.
