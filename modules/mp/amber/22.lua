ea323217c4cf7a72d27f44386f324c9615dbd856
John LaGrone <jlagrone@gmail.com>
2023-06-05 08:21:35 -0500

    add module for amber 22

---
+++ modules/mp/amber/22.lua
@@ -0,0 +1,10 @@
+whatis("Amber")
+family("amber")
+
+source_sh("/hpc/mp/apps/amber/amber22/amber.sh")
+load("dev")
+load("hpcx")
+load("cuda-11.7.0-gcc-10.3.0-vbhdtgc"
+load("fftw-3.3.10-gcc-10.3.0-ste4ien"
+load("openblas-0.3.20-gcc-10.3.0-nqwcc2e"
+load("nccl-2.11.4-1-gcc-10.3.0-rvdzi4n"
