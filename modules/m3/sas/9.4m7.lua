d45c9aeb926553f88cdb6bfa4647be8b124d948a
John LaGrone <jlagrone@gmail.com>
2023-06-13 14:50:15 -0500

    add sas module

---
+++ modules/m3/sas/9.4m7.lua
@@ -0,0 +1,4 @@
+whatis("loads SAS v9.4 executables in current environment")
+setenv("SASHOME","/hpc/m3/apps/sas/9.4_m7")
+prepend_path("PATH", "/hpc/m3/apps/sas/9.4_m7/SASFoundation/9.4")
+prepend_path("MANPATH", "/hpc/m3/apps/sas/9.4_m7/SASFoundation/9.4/utilities/man")
