bcaed046b909e8ad0f90b174cacd4e09948179f0
John LaGrone <jlagrone@gmail.com>
2023-06-15 11:42:06 -0500

    add build files for an Alma based desktop

---
+++ containers/remote_desktop/build_almalinux_desktop.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+# load apptainer
+module purge
+module load apptainer
+unset APPTAINER_BIND
+
+# set version
+VERSION=1.0.0
+
+# TODO: gpu enabled?
+
+CONTAINER_NAME=almalinux_remote_desktop_${VERSION}.sif
+
+# check if this version already exits
+CLUSTER=$(scontrol show config | grep ClusterName | grep -oP '= \K.+')
+if [ "$CLUSTER" = "nvidia" ]; then
+  CLUSTER="mp"
+fi
+
+DESTINATION=/hpc/${CLUSTER}/containers/remote_desktop/${CONTAINER_NAME}
+
+if [ -f "$DESTINATION" ]; then
+
+  read -p "$DESTINATION already exists. Overwrite y/N? " yn
+  case $yn in
+     [Yy]* ) echo "Overwriting existing file";;
+     * ) exit;;
+  esac
+fi
+
+# build the container
+apptainer build --fakeroot ${CONTAINER_NAME} almalinux_desktop.def
+
+# move container to /hpc/{sys}/containers/
+mkdir -p /hpc/${CLUSTER}/containers/remote_desktop
+mv ${CONTAINER_NAME} ${DESTINATION}
