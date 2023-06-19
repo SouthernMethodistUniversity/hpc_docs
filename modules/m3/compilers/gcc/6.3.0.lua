5ec223b48f59865e1bb6784c97985c129c9e663d
John LaGrone <jlagrone@gmail.com>
2023-06-19 15:11:48 -0500

    actually load the module. Add warning about optimization

--- modules/m3/compilers/gcc/6.3.0.lua
+++ modules/m3/compilers/gcc/6.3.0.lua
@@ -1,6 +1,11 @@
 whatis("Provides access to GCC 6.3.0 compiler suite, libraries, and applications.")
 family("compiler")
 
+LmodMessage("\n\nGCC 6.3.0 is available for compatibility reasons.\nIt lacks architecture dependent optimizations that are required to take full advantage of M3 \n")
+
+depends_on("spack")
+always_load("gcc-11.2.0/gcc/6.3.0-uyj5hrn")
+
 append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-zen/gcc-6.3.0")
 
 -- For OpenMPI
