f2e350cdbd7c6a728c1e00a2400145df71e137f8
John LaGrone <jlagrone@gmail.com>
2023-06-23 11:32:49 -0500

    add cplex module

---
+++ modules/m3/applications/cplex/22.1.1.lua
@@ -0,0 +1,27 @@
+whatis("IMB ILOG CPLEX Optimization Studio 22.1.1")
+family("cplex")
+conflict("cplex")
+
+setenv("CPLEX_HOME", "/hpc/m3/apps/cplex/CPLEX_Studio2211")
+
+append_path("PATH", "/hpc/m3/apps/cplex/CPLEX_Studio2211/cplex/bin/x86-64_linux/")
+append_path("PATH", "/hpc/m3/apps/cplex/CPLEX_Studio2211/cpoptimizer/bin/x86-64_linux")
+append_path("PATH", "/hpc/m3/apps/cplex/CPLEX_Studio2211/opl/bin/x86-64_linux")
+
+append_path("LD_LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/cplex/lib/x86-64_linux/static_pic")
+append_path("LD_LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/cplex/bin/x86-64_linux")
+append_path("LD_LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/cpoptimizer/lib/x86-64_linux/static_pic")
+append_path("LD_LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/cpoptimizer/bin/x86-64_linux")
+append_path("LD_LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/opl/lib/x86-64_linux/static_pic")
+append_path("LD_LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/opl/bin/x86-64_linux")
+
+append_path("LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/cplex/lib/x86-64_linux/static_pic")
+append_path("LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/cplex/bin/x86-64_linux")
+append_path("LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/cpoptimizer/lib/x86-64_linux/static_pic")
+append_path("LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/cpoptimizer/bin/x86-64_linux")
+append_path("LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/opl/lib/x86-64_linux/static_pic")
+append_path("LIBRARY_PATH","/hpc/m3/apps/cplex/CPLEX_Studio2211/opl/bin/x86-64_linux")
+
+append_path("CPATH", "/hpc/m3/apps/cplex/CPLEX_Studio2211/cplex/include")
+append_path("CPATH", "/hpc/m3/apps/cplex/CPLEX_Studio2211/cpoptimizer/include")
+append_path("CPATH", "/hpc/m3/apps/cplex/CPLEX_Studio2211/opl/include")
