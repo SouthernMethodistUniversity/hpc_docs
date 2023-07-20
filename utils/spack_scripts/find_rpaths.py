f0b9416b71f80a7e6f80c54d1881779b7dc864fa
John LaGrone <jlagrone@gmail.com>
2023-07-20 12:34:01 -0500

    fix typos

--- utils/spack_scripts/find_rpaths.py
+++ utils/spack_scripts/find_rpaths.py
@@ -31,9 +31,9 @@ def get_spack_dep_hashes(pkg):
   if err:
     sys.exit(-1)
 
-  deps = dep_string.split(" ")
-  for i in range(0, len(deps):
-    deps[i] = '\' + deps[i]
+  deps = dep_string.strip().split(" ")
+  for i in range(0, len(deps)):
+    deps[i] = '/' + deps[i]
 
   return deps
 
@@ -59,33 +59,7 @@ else:
   for p in args.package:
     pkg = pkg + p + ' '
 
+# get the dependencies
+deps = get_spack_dep_hashes(pkg)
 
-# try to find the package dependencies
-command_base = 'spack dependents --installed '
-
-command = command_base + pkg
-
-run_cmd = shlex.split(command)
-proc = subprocess.Popen(run_cmd, shell=False, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-
-dep_string = ''
-err=False
-while (True):
-  line = proc.stdout.readline()
-  if (line == ""): break
-  if 'Error' in line:
-    print('Error:')
-    print(proc.stdout)
-    err = True
-  elif '--' in line:
-    continue
-  else:
-    dep_string = dep_string + line.strip().split(" ")[0] + ' '
-    if err:
-      print(line)
-
-if err:
-  sys.exit(-1)
-
-print("dep_string: ", dep_string)
+print("deps: ", deps)
