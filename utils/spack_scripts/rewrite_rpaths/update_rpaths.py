cb52e803d8ac0bc60bf0b6d98a65c1f9996648fe
John LaGrone <jlagrone@gmail.com>
2023-07-21 12:59:54 -0500

    get locations / libraries that need to be updated

--- utils/spack_scripts/rewrite_rpaths/update_rpaths.py
+++ utils/spack_scripts/rewrite_rpaths/update_rpaths.py
@@ -37,5 +37,75 @@ def get_needed_libs(pkg):
 
   return needs
 
-print(get_rpath('/users/jlagrone/repos/hpc_docs/utils/spack_scripts/rewrite_rpaths/test_lib.so'))
-print(get_needed_libs('/users/jlagrone/repos/hpc_docs/utils/spack_scripts/rewrite_rpaths/test_lib.so'))
+def get_new_lib_path(lib, prefix):
+  for subdir, dirs, files in os.walk(prefix):
+    for file in files:
+      if str(file) == str(lib):
+        f = prefix.joinpath(subdir).joinpath(file)
+        return f
+
+  return ''
+
+# Set up command line options
+parser = argparse.ArgumentParser(
+    description='Update rpaths for package dependents in a specified list.')
+parser.add_argument('-i', '--packagepaths',
+                    help="File with package(s) to update (1 per line)",
+                    type=pathlib.Path)
+parser.add_argument('-o', '--outfile',
+                    help="File to save output to (defaults to rpath_update_{date}.json)",
+                    type=pathlib.Path)
+parser.add_argument('-v', '--verbose',
+                    help="output debugging information",
+                    default=False, action='store_true')
+parser.add_argument('-n', '--newpath',
+                    help="Prefix for the new library path",
+                    type=pathlib.Path)
+# parse the input
+args = parser.parse_args()
+
+if args.newpath is None:
+  print("No prefix specified to update rpaths specified")
+  parser.print_help()
+  sys.exit(-1)
+else:
+  prefix = args.newpath
+
+if args.packagepaths is None:
+  print("No file with packages to update specified")
+  parser.print_help()
+  sys.exit(-1)
+else:
+  pkg_file = args.packagepaths
+
+# read in package locations
+with open(pkg_file, 'r') as f:
+  pkg_paths = f.readlines()
+  for i in range(0, len(pkg_paths)):
+    pkg_paths[i] = pkg_paths[i].strip()
+
+if args.verbose:
+  print("pkg_paths: \n", pkg_paths)
+
+# loop over the packages to update
+for p in pkg_paths:
+
+  # get the rpath and needed libs for the current package
+  cur_rpath = get_rpath(p)
+  cur_needs = get_needed_libs(p)
+
+  if args.verbose:
+    print("cur_rpath: \n", cur_rpath)
+    print("cur_needs: \n", cur_needs)
+
+  # check to see if one of the needed libs is in the new path
+  update_list = []
+  for need in cur_needs:
+    need_path = get_new_lib_path(need, prefix)
+    if need_path:
+      tmp = [need, need_path]
+      update_list.append(tmp)
+
+  if args.verbose:
+    print("update_list: \n", update_list)
+
