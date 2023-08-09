c2517a082cf799d865a1d795dee2f9cbb00ef6ca
John R Lagrone <jlagrone@slogin-01.cm.cluster>
2023-08-09 12:53:50 -0500

    remove old modules

--- modules/mp/conda.lua
+++
@@ -1,3 +0,0 @@
-source_sh('bash', '/hpc/mp/apps/conda/etc/profile.d/conda.sh')
-source_sh('bash', '/hpc/mp/apps/conda/etc/profile.d/mamba.sh')
-
--- modules/mp/spack.lua
+++
@@ -1,2 +0,0 @@
-source_sh('bash', '/hpc/mp/spack/share/spack/setup-env.sh')
-
