dc7b14a4a05ace600095ae9ca4f228b9fc1154de
John R Lagrone <jlagrone@slogin-01.cm.cluster>
2023-08-09 13:52:56 -0500

    move module files and update load path

--- modules/mp/compilers/gcc/11.2.0.lua
+++ modules/mp/compilers/gcc/11.2.0.lua
@@ -1,5 +1,6 @@
 whatis("Provides access to GCC 11.2.0 compiler suite, libraries, and applications.")
 family("compiler")
 
+append_path("MODULEPATH", "/users/jlagrone/repos/hpc_docs/modules/mp/cuda/gcc-11.2.0/")
 append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.2.0/")
 
