de4189bc1adce5212c564a9b74f9e6ec4cb4f276
John R Lagrone <jlagrone@slogin-01.cm.cluster>
2023-08-09 13:41:36 -0500

    add gcc with cuda 11.2 and 11.8

---
+++ modules/mp/compilers/gcc/11.2.0.lua
@@ -0,0 +1,5 @@
+whatis("Provides access to GCC 11.2.0 compiler suite, libraries, and applications.")
+family("compiler")
+
+append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.2.0/")
+
---
+++ modules/mp/compilers/gcc/cuda/11.2.2.lua
@@ -0,0 +1,6 @@
+whatis("Provides access to Cuda 11.2.2.")
+family("cuda")
+
+always_load("cuda/11.2.2-hzxxgtc")
+-- append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/cuda-11.2.2/gcc-11.2.0")
+
