df2c5c4c38de7ccc9d5043b0b3da48d78edcc88f
John R Lagrone <jlagrone@slogin-01.cm.cluster>
2023-08-09 18:06:54 -0500

    remove cuda modules

--- modules/mp/cuda/gcc-11.2.0/cuda/11.8.0.lua
+++
@@ -1,6 +0,0 @@
-whatis("Provides access to Cuda 11.2.2.")
-family("cuda")
-
-always_load("cuda/11.8.0-hiuxb43")
-append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/cuda-11.8.0-gcc-11.2.0/")
-
