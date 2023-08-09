f774fac7a3c0d9641fe0d1f637ce85df93f3c692
John R Lagrone <jlagrone@slogin-01.cm.cluster>
2023-08-09 18:11:09 -0500

    add cuda and regular package paths

--- modules/mp/compilers/gcc/11.2.0.lua
+++ modules/mp/compilers/gcc/11.2.0.lua
@@ -1,5 +1,8 @@
 whatis("Provides access to GCC 11.2.0 compiler suite, libraries, and applications.")
 family("compiler")
 
-append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.2.0/")
+-- cuda
+append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/cuda/gcc-11.2.0/")
 
+-- generic packages / dependencies
+append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.2.0/")
