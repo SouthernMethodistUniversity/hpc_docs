bed4a4708924ce22c64fbc70120492c7879baa83
John R Lagrone <jlagrone@slogin-01.cm.cluster>
2023-08-11 13:26:20 -0500

    update paths

--- modules/mp/compilers/gcc/11.2.0.lua
+++ modules/mp/compilers/gcc/11.2.0.lua
@@ -2,7 +2,7 @@ whatis("Provides access to GCC 11.2.0 compiler suite, libraries, and application
 family("compiler")
 
 -- cuda
-append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/cuda/gcc-11.2.0/")
+append_path("MODULEPATH", "/hpc/mp/spack_modules/linux-ubuntu22.04-zen2/cuda/gcc-11.2.0/")
 
 -- generic packages / dependencies
-append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/gcc-11.2.0/")
+append_path("MODULEPATH", "/hpc/mp/spack_modules/linux-ubuntu22.04-zen2/gcc-11.2.0/")
