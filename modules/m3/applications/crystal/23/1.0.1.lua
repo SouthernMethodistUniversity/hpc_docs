524985cd80b8cec86a0ce92fb8324916072b3515
Robert Kalescky <rkalescky@smu.edu>
2023-09-27 15:43:45 -0500

    Added CRYSTAL23.

--- modules/m3/applications/crystal/23/1.0.1.lua
+++ modules/m3/applications/crystal/23/1.0.1.lua
@@ -5,8 +5,8 @@ if string.match(groups, "crystal") == nil then
 else
    family("crystal")
    conflict("crystal")
-   load("intel/2023.1")
-   load("mpi", "mkl")
-   prepend_path("PATH","/hpc/m3/apps/crystal/23/bin/Linux-ifort_i64_omp/v1.0.1")
+   load("gcc", "openmpi")
+   append_path("LD_LIBRARY_PATH", "/hpc/m3/apps/intel/oneapi/2023.1/compiler/2023.1.0/linux/compiler/lib/intel64_lin")
+   prepend_path("PATH","/hpc/m3/apps/crystal/23/v1.0.1")
 end
 
