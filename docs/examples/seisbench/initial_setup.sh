57d0cd84eeb8feef4ab3433b90a80d156f89252b
Robert Kalescky <rkalescky@smu.edu>
2023-09-29 08:18:11 -0500

    Removed M2 specific Python versions.

--- docs/examples/seisbench/initial_setup.sh
+++ docs/examples/seisbench/initial_setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-module purge
-module load gcc-9.2.lua spack.lua python-3.8.6-gcc-9.2.0-7vsoogc
 env_dir=$HOME/.venv/seisbench
 python3 -m venv $env_dir
 source $env_dir/bin/activate
