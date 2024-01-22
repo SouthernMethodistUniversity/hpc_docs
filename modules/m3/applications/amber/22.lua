ad4e7452be798165c009c59772901856716a558b
John LaGrone <jlagrone@gmail.com>
2024-01-22 13:53:23 -0600

    Use new openMPI. Slurm / PMIX update broke mpi version in module

--- modules/m3/applications/amber/22.lua
+++ modules/m3/applications/amber/22.lua
@@ -4,7 +4,7 @@ load("gcc/11.2.0")
 load("openblas/0.3.21-s5husbk",
      "fftw/3.3.10-gz7qiki",
      "zlib/1.2.13-u7kx7ln",
-     "openmpi/4.1.4-7al4h2x",
+     "openmpi/4.1.6-a4ksrza",
      "cmake/3.24.3-2buptpk",
      "boost/1.80.0-l5cj6ju",
      "bzip2/1.0.8-ujseine")
