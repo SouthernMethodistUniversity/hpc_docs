7a516644f98c4a0acec999f733227f66799d79e4
John LaGrone <jlagrone@gmail.com>
2024-04-29 10:49:37 -0500

    add module for new matlab

---
+++ modules/m3/applications/matlab/r2024a.lua
@@ -0,0 +1,5 @@
+whatis("MATLAB R2024a")
+family("matlab")
+prepend_path("PATH","/hpc/m3/apps/Matlab/r2024a/bin")
+prepend_path("LD_LIBRARY_PATH","/hpc/m3/apps/Matlab/r2024a/bin/glnxa64/")
+setenv("MATLABROOT","/hpc/m3/apps/Matlab/r2024a")
