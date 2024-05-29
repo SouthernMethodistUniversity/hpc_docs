f0cc3ebcd9d2c5e2ba638db274c8e8fa9a6f34de
John LaGrone <jlagrone@gmail.com>
2024-05-29 11:21:59 -0500

    add compatibility libs for cuda 12.4

---
+++ modules/mp/cuda_compatibility/cuda_compat_libs/cuda_12.4_compat.lua
@@ -0,0 +1,4 @@
+append_path("LD_PRELOAD", "/hpc/mp/apps/nvidia/compatibility_drivers/12.4/libcuda.so.550.54.15")
+append_path("LD_PRELOAD", "/hpc/mp/apps/nvidia/compatibility_drivers/12.4/libcudadebugger.so.550.54.15")
+append_path("LD_PRELOAD", "/hpc/mp/apps/nvidia/compatibility_drivers/12.4/libnvidia-nvvm.so.550.54.15")
+append_path("LD_PRELOAD", "/hpc/mp/apps/nvidia/compatibility_drivers/12.4/libnvidia-ptxjitcompiler.so.550.54.15")
