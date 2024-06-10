7dd23b31dbee9998a6bb8da44ab07dc80c5ca14d
John LaGrone <jlagrone@gmail.com>
2024-06-10 13:47:14 -0500

    tweaking checks to try to get them working

--- ood/common/CheckEnabled.rb
+++ ood/common/CheckEnabled.rb
@@ -1,6 +1,6 @@
 class CheckEnabled
 
-  @cache = ActiveSupport::Cache::FileStore.new("/users/#{User.new.name}/.cache/OpenOnDemand/", :expires_in => 1.minute)
+#  @cache = ActiveSupport::Cache::FileStore.new("/users/#{User.new.name}/.cache/OpenOnDemand/", :expires_in => 1.minute)
 
   def self.IsEnabled()
     begin
@@ -9,12 +9,19 @@ class CheckEnabled
       o, status = Open3.capture2e(script)
       tmp_output = o.split("\n")
       output = true
+      File.open('/users/jlagrone/ood_test.log', 'w') do |fo|
+      old_std = $stdout
+      $stdout = fo
+      puts "tmp_output: #{tmp_output}"
       tmp_output.each do |q|
         tmp = q.strip
-        if tmp.include? "/sbin/nologin"
+        if tmp.include? "jlagrone" #"/sbin/nologin"
+          puts "tmp: #{tmp}"
           output = false
         end
       end
+      end
+      $stdout = old_std
 
       return output
     end
@@ -22,8 +29,8 @@ class CheckEnabled
 
   def self.Enabled
     begin
-      @Enabled = @cache.fetch("#{User.new.name}/enabled", race_condition_ttl: 30.seconds) do
-        self.IsEnabled()
+      @Enabled = self.IsEnabled() #@cache.fetch("#{User.new.name}/enabled", race_condition_ttl: 30.seconds) do
+        #self.IsEnabled()
       end
       return @Enabled
     end
