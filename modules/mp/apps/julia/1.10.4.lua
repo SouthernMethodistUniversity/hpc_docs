bfedbe7ba27cb0190ce85c905535aa0dccf85673
John LaGrone <jlagrone@gmail.com>
2024-07-05 08:46:01 -0500

    add julia 1.10.4

---
+++ modules/mp/apps/julia/1.10.4.lua
@@ -0,0 +1,8 @@
+whatis("loads Julia 1.10.4 environment")
+family("julia")
+conflict("julia")
+local root = "/hpc/mp/apps/julia/julia-1.10.4"
+prepend_path("PATH", pathJoin(root, "bin"))
+prepend_path("MANPATH", pathJoin(root, "share/man"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib"))
+
