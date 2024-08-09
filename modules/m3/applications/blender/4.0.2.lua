1c95bd770f38fccb8d8bbcc3b48be09f8b6f1212
John LaGrone <jlagrone@gmail.com>
2024-08-09 11:19:07 -0500

    add help to blender

--- modules/m3/applications/blender/4.0.2.lua
+++ modules/m3/applications/blender/4.0.2.lua
@@ -3,3 +3,10 @@ family("blender")
 conflict("blender")
 
 append_path("PATH", "/hpc/m3/apps/blender/4.0.2")
+
+help([[Name: Blender
+Version: 4.0.2
+Website: https://www.blender.org/
+
+Blender is the free and open source 3D creation suite. It supports the entirety of the 3D pipeline—modeling, rigging, animation, simulation, rendering, compositing and motion tracking, even video editing and game creation.
+]])
