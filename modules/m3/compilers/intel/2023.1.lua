e55158c2ba984cf411f7191117d8cbba9bbbe88f
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:37:35 -0500

    add help for intel compilers

--- modules/m3/compilers/intel/2023.1.lua
+++ modules/m3/compilers/intel/2023.1.lua
@@ -4,3 +4,9 @@ family("compiler")
 append_path("MODULEPATH", "/hpc/m3/apps/intel/oneapi/2023.1/modulefiles")
 load("compiler/latest")
 
+help([[Name: Intel OneAPI Compiler Suite
+Version: 2023.1
+Website: https://www.intel.com/content/www/us/en/developer/tools/oneapi/overview.html
+
+The Intel® oneAPI DPC++/C++ Compiler gives developers even more confidence that their code is portable and reliably performant across existing and future CPU, GPU, and AI accelerator architectures.
+]])
--- modules/m3/compilers/intel/oneapi/2023.2.lua
+++ modules/m3/compilers/intel/oneapi/2023.2.lua
@@ -13,3 +13,9 @@ append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/cud
 -- generic packages / dependencies
 append_path("MODULEPATH", "/hpc/m3/modules/spack/linux-ubuntu22.04-x86_64_v3/oneapi-2023.2.0/")
 
+help([[Name: Intel OneAPI Compiler Suite
+Version: 2023.2
+Website: https://www.intel.com/content/www/us/en/developer/tools/oneapi/overview.html
+
+The Intel® oneAPI DPC++/C++ Compiler gives developers even more confidence that their code is portable and reliably performant across existing and future CPU, GPU, and AI accelerator architectures.
+]])
