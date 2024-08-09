98fd84e9638b6029d5428d5d6eeedf5fd7319e7b
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:11:29 -0500

    add molpro help

--- modules/m3/applications/molpro/2022.3.lua
+++ modules/m3/applications/molpro/2022.3.lua
@@ -3,3 +3,9 @@ family("molpro")
 conflict("molpro")
 prepend_path("PATH","/hpc/m3/apps/molpro/2022.3/bin")
 
+help([[Name: Molpro
+Version: 2022.3
+Website: https://www.molpro.net/
+
+Molpro is a comprehensive system of ab initio programs for advanced molecular electronic structure calculations
+]])
