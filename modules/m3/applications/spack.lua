2706e3f5bf86898c9f1446ff58dc97af99b6f5b8
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:34:31 -0500

    add help for spack

--- modules/m3/applications/spack.lua
+++ modules/m3/applications/spack.lua
@@ -1,3 +1,10 @@
 whatis("Spack")
 family("spack")
 source_sh('bash', '/hpc/m3/spack/share/spack/setup-env.sh')
+
+help([[Name: Spack
+Version: several
+Website: https://spack.io/
+
+pack is a package manager for supercomputers, Linux, and macOS.
+]])
