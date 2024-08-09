c236784e166e89a0c1f8ad6e66316ac4e32a0fcd
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:33:49 -0500

    add conda help

--- modules/m3/applications/conda.lua
+++ modules/m3/applications/conda.lua
@@ -2,3 +2,11 @@ whatis("Anaconda Python Distribution")
 family("conda")
 source_sh('bash', '/hpc/m3/apps/conda/etc/profile.d/conda.sh')
 source_sh('bash', '/hpc/m3/apps/conda/etc/profile.d/mamba.sh')
+
+
+help([[Name: Conda
+Version: several
+Website: https://docs.conda.io/projects/conda/en/stable/
+
+Conda provides package, dependency, and environment management for any language.
+]])
