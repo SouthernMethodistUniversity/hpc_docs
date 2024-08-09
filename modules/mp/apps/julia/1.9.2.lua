ee686b57cbafa51e22393e8d0b5d3c2b22068d22
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:49:27 -0500

    add Julia help

--- modules/mp/apps/julia/1.9.2.lua
+++ modules/mp/apps/julia/1.9.2.lua
@@ -5,3 +5,10 @@ local root = "/hpc/mp/apps/julia/1.9.2"
 prepend_path("PATH", pathJoin(root, "bin"))
 prepend_path("MANPATH", pathJoin(root, "share/man"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib"))
+
+help([[Name: Julia
+Version: 1.9.2
+Website: https://julialang.org/
+
+The Julia Programming Language
+]])
