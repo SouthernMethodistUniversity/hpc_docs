448dc1972c8f134446abc96c03e94298a9751df1
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:47:19 -0500

    add help for conda

--- modules/mp/apps/conda.lua
+++ modules/mp/apps/conda.lua
@@ -1,2 +1,9 @@
 source_sh('bash', '/hpc/mp/apps/conda/etc/profile.d/conda.sh')
 source_sh('bash', '/hpc/mp/apps/conda/etc/profile.d/mamba.sh')
+
+help([[Name: Conda
+Version: several
+Website: https://docs.conda.io/projects/conda/en/stable/
+
+Conda provides package, dependency, and environment management for any language.
+]])
