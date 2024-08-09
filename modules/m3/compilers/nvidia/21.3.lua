af6d4332d100b441d900bcb76aa41616cf114ec7
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:38:33 -0500

    add help for nvhpc

--- modules/m3/compilers/nvidia/21.3.lua
+++ modules/m3/compilers/nvidia/21.3.lua
@@ -4,3 +4,9 @@ family("compiler")
 append_path("MODULEPATH", "/hpc/m3/apps/nvidia/hpc-sdk/21.3/modulefiles")
 load("nvhpc/21.3")
 
+help([[Name: NVIDIA HPC SDK
+Version: 21.3
+Website: https://developer.nvidia.com/hpc-sdk
+
+The NVIDIA HPC Software Development Kit (SDK) includes the proven compilers, libraries and software tools essential to maximizing developer productivity and the performance and portability of HPC applications.
+]])
