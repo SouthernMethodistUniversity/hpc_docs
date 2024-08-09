d5f89dfd51a6eff9e25ce40dd6b5226bc1e8d183
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:01:13 -0500

    add help for gurobi module

--- modules/m3/applications/gurobi/11.0.1.lua
+++ modules/m3/applications/gurobi/11.0.1.lua
@@ -8,3 +8,11 @@ setenv("GRB_LICENSE_FILE", "/hpc/m3/apps/gurobi/11.0.1/linux64/client.lic")
 append_path("LD_LIBRARY_PATH","/hpc/m3/apps/gurobi/11.0.1/linux64/lib")
 append_path("CPATH", "/hpc/m3/apps/gurobi/11.0.1/linux64/include")
 append_path("LIBRARY_PATH","/hpc/m3/apps/gurobi/11.0.1/linux64/lib")
+
+help([[Name: Gurobi
+Version: 11.0.1
+Website: https://www.gurobi.com/
+License Owners : free academic site license (Merlin)
+
+Gurobi optimization software
+]])
