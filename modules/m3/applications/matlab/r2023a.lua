7deb0b32dadce316082185a8c2f4893086901fff
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:06:41 -0500

    add help for matlab

--- modules/m3/applications/matlab/r2023a.lua
+++ modules/m3/applications/matlab/r2023a.lua
@@ -3,3 +3,13 @@ family("matlab")
 prepend_path("PATH","/hpc/m3/apps/matlab/r2023a/bin")
 prepend_path("LD_LIBRARY_PATH","/hpc/m3/apps/matlab/r2023a/bin/glnxa64/")
 setenv("MATLABROOT","/hpc/m3/apps/matlab/r2023a")
+
+
+help([[Name: Matlab
+Version: 2023a
+Website: https://www.mathworks.com/
+License Owners: SMU site license
+
+
+MATLAB is a programming and numeric computing platform used by millions of engineers and scientists to analyze data, develop algorithms, and create models.
+]])
