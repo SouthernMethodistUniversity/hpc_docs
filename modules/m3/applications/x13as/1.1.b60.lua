6210b3830a0eade2ec214d569b79aad8f254a0de
John LaGrone <jlagrone@gmail.com>
2024-08-09 12:32:49 -0500

    add help for x13as

--- modules/m3/applications/x13as/1.1.b60.lua
+++ modules/m3/applications/x13as/1.1.b60.lua
@@ -1,5 +1,11 @@
 whatis("x13as 1.1.b60")
 family("x13as")
 
-
 append_path("PATH", "/hpc/m3/apps/x13as/1.1.b60")
+
+help([[Name: x13as
+Version: 1.1.b60
+Website: https://www.census.gov/data/software/x13as.html
+
+X-13ARIMA-SEATS Seasonal Adjustment Program
+]])
