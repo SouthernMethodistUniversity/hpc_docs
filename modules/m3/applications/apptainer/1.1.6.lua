bce8d4e3ef7b55c49371f92d372c5c587a1d5812
John LaGrone <jlagrone@gmail.com>
2024-09-17 13:46:28 -0500

    change /projects mount to /work/projects:/projects

--- modules/m3/applications/apptainer/1.1.6.lua
+++ modules/m3/applications/apptainer/1.1.6.lua
@@ -3,7 +3,7 @@ family("apptainer")
 prepend_path("PATH","/hpc/sys/apps/apptainer/current/bin")
 source_sh('bash', '/hpc/sys/apps/apptainer/current/share/bash-completion/completions/apptainer')
 source_sh('bash', '/hpc/sys/apps/apptainer/current/share/bash-completion/completions/singularity')
-setenv("APPTAINER_BIND","/tmp,/hpc,/lustre/work/client,/lustre/scratch/client,/work,/projects,/cm/local/apps/slurm,/var/lib/sss,/etc/passwd")
+setenv("APPTAINER_BIND","/tmp,/hpc,/lustre/work/client,/lustre/scratch/client,/work,/work/projects:/projects,/cm/local/apps/slurm,/var/lib/sss,/etc/passwd")
 
 help([[Name: Apptainer
 Version: 1.1.6
