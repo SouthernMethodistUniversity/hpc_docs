f90bc37e9dd593607c918d78774e45de0fcb12a4
John LaGrone <jlagrone@gmail.com>
2024-10-01 14:45:35 -0500

    update mpi for cfour

--- modules/m3/applications/cfour/2.1/mpi.lua
+++ modules/m3/applications/cfour/2.1/mpi.lua
@@ -5,7 +5,7 @@ local base = "/hpc/m3/apps/cfour/2.1"
 local root = pathJoin(base, mpi)
 local bin  = pathJoin(root, "bin")
 
-load("gcc/11.2.0", "openmpi/4.1.4-7al4h2x", "openblas/0.3.21-iofurbe")
+load("gcc/11.2.0", "openmpi/4.1.6-vfi4iwj", "openblas/0.3.21-iofurbe")
 prepend_path("PATH", bin)
 
 help([[Name: CFOUR
