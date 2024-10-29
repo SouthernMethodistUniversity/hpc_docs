c39b95d0fd184b75634d9057875c47cf09181efd
John LaGrone <jlagrone@gmail.com>
2024-10-29 16:39:20 -0500

    add js to disable submission on remote desktop if there are no valid accounts

---
+++ ood/combined_desktop/form.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/*
+ * Invoke the functions when the DOM is ready.
+ */
+$(document).ready(function () {
+
+  if ($('#batch_connect_session_context_slurm_accounts option').length == 0) {
+    $('input[name="commit"]').prop("disabled",true);
+  }
+
+});
