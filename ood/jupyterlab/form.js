39d35c084e28936ce8b4383841e4cd9ab52cb5b7
John LaGrone <jlagrone@gmail.com>
2024-10-29 16:41:02 -0500

    update js to disable submit buttons if there are no valid accounts

--- ood/jupyterlab/form.js
+++ ood/jupyterlab/form.js
@@ -14,4 +14,8 @@ $(document).ready(function () {
       }
     });
   }
+
+  if ($('#batch_connect_session_context_slurm_accounts option').length == 0) {
+    $('input[name="commit"]').prop("disabled",true);
+  }
 });
