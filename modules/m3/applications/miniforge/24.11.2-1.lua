3ed9a25c13399187a30da58af68cb3cefe89e536
John LaGrone <jlagrone@gmail.com>
2025-01-08 16:49:04 -0600

    add extensions to show conda and mamba version to miniforge

--- modules/m3/applications/miniforge/24.11.2-1.lua
+++ modules/m3/applications/miniforge/24.11.2-1.lua
@@ -1,8 +1,9 @@
 whatis("Miniforge")
 family("conda")
+extensions("conda/24.11.2", "mamba/1.5.12")
+
 source_sh('bash', '/hpc/m3/apps/miniforge/24.11.2-1/etc/profile.d/conda.sh')
 source_sh('bash', '/hpc/m3/apps/miniforge/24.11.2-1/etc/profile.d/mamba.sh')
-
 
 help([[Name: Miniforge
 Version: 24.11.2-1
