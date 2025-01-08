19b70c279571d54611ab475ccea889dfddeda385
John LaGrone <jlagrone@gmail.com>
2025-01-08 12:37:05 -0600

    add module for miniforge

---
+++ modules/m3/applications/miniforge/24.11.2-1.lua
@@ -0,0 +1,12 @@
+whatis("Miniforge")
+family("conda")
+source_sh('bash', '/hpc/m3/apps/miniforge/24.11.2-1/etc/profile.d/conda.sh')
+source_sh('bash', '/hpc/m3/apps/miniforge/24.11.2-1/etc/profile.d/mamba.sh')
+
+
+help([[Name: Miniforge
+Version: 24.11.2-1
+Website: https://github.com/conda-forge/miniforge
+
+Miniforge provides minimal installations for Conda and Mamba with conda-forge pre-configured.
+]])
