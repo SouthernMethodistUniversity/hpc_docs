d820c7932ab0073f7044fe3703d0eba2773871b3
John LaGrone <jlagrone@gmail.com>
2025-01-29 09:00:51 -0600

    add module for comsol 6.3

---
+++ modules/m3/applications/comsol/6.3.lua
@@ -0,0 +1,12 @@
+whatis("COMSOL 6.3")
+family("comsol")
+conflict("comsol")
+prepend_path("PATH","/hpc/m3/apps/comsol/comsol63/multiphysics/bin")
+
+help([[Name: Comsol  
+Version: 6.3
+Website: https://www.comsol.com/
+License Owners : SMU?
+
+COMSOL Multiphysics is a finite element analyzer, solver, and simulation software package for various physics and engineering applications, especially coupled phenomena and multiphysics.
+]])
