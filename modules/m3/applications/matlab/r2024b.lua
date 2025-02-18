4d177d370768fe86b93e2cd09707add1f902ce0b
John LaGrone <jlagrone@gmail.com>
2025-02-18 17:24:00 -0600

    add 2024b matlab module

---
+++ modules/m3/applications/matlab/r2024b.lua
@@ -0,0 +1,14 @@
+whatis("MATLAB R2024b")
+family("matlab")
+prepend_path("PATH","/hpc/m3/apps/Matlab/r2024b/bin")
+prepend_path("LD_LIBRARY_PATH","/hpc/m3/apps/Matlab/r2024b/bin/glnxa64/")
+setenv("MATLABROOT","/hpc/m3/apps/Matlab/r2024b")
+
+help([[Name: Matlab
+Version: 2024b
+Website: https://www.mathworks.com/
+License Owners: SMU site license
+
+
+MATLAB is a programming and numeric computing platform used by millions of engineers and scientists to analyze data, develop algorithms, and create models.
+]])
