d0e4323ffc812e8f56bac6fe6e6bf85519c69a71
John LaGrone <jlagrone@gmail.com>
2025-09-05 08:47:32 -0500

    add new ampl

---
+++ modules/m3/applications/ampl/20250811.lua
@@ -0,0 +1,13 @@
+whatis("AMPL")
+family("ampl")
+conflict("ampl")
+
+append_path("PATH", "/hpc/m3/apps/ampl/ampl_20250811")
+
+help([[Name: AMPL
+Version: ampl_20250811
+Website: https://ampl.com/
+License Owners : Merlin / Teaching Eval Version
+
+AMPL is an algebraic modeling language to describe and solve high-complexity problems for large-scale mathematical computing.
+]])
