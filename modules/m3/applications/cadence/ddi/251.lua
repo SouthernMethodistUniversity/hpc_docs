9d2c5121b8dbc9986cdcb9a0232b0ab5db11b0b1
John LaGrone <jlagrone@gmail.com>
2025-10-03 15:43:55 -0500

    add more cadence modules

---
+++ modules/m3/applications/cadence/ddi/251.lua
@@ -0,0 +1,17 @@
+whatis("Cadence Digital Design and Implementation 25.1")
+family("ddi")
+conflict("ddi")
+extensions("genus/251, innovus/251, jstudio/251")
+
+
+setenv("LM_LICENSE_FILE","/hpc/m3/apps/cadence/license2.dat")
+
+prepend_path("PATH","/hpc/m3/apps/cadence/DDI251/bin")
+
+help([[Name: Cadence Digital Design and Implementation  
+Version: 25.1
+Website: https://www.cadence.com/
+License Owners : SMU?
+
+Cadence Digital Design and Implementation Suite.
+]])
---
+++ modules/m3/applications/cadence/ssv/251.lua
@@ -0,0 +1,18 @@
+whatis("Cadence Silicon Signoff and Verification 25.1")
+family("ssv")
+conflict("ssv")
+extensions("tempus/251, voltus/251, litmus/251")
+
+
+setenv("LM_LICENSE_FILE","/hpc/m3/apps/cadence/license2.dat")
+
+prepend_path("PATH","/hpc/m3/apps/cadence/SSV251/bin")
+prepend_path("PATH","/hpc/m3/apps/cadence/SSV251/bin/tools")
+
+help([[Name: Cadence Silicon Signoff and Verification 
+Version: 25.1
+Website: https://www.cadence.com/
+License Owners : SMU?
+
+Cadence Silicon Signoff and Verification  Suite.
+]])
