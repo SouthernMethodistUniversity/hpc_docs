33e9e907307793cf487720b736aae30c72869b06
John LaGrone <jlagrone@gmail.com>
2025-10-03 08:40:04 -0500

    add freecad module

---
+++ modules/m3/applications/freecad/1.0.2.lua
@@ -0,0 +1,13 @@
+whatis("FreeCAD 1.0.2")
+family("freecad")
+conflict("freecad")
+
+append_path("PATH", "/hpc/m3/apps/freecad/1.0.2")
+
+help([[Name: FreeCAD
+Version: 1.0.2
+Website: https://www.freecad.org/
+License Owners : Open Source, GPLv2
+
+FreeCAD is an open-source parametric 3D modeler made primarily to design real-life objects of any size. Parametric modeling allows you to easily modify your design by going back into your model history and changing its parameters.
+]])
