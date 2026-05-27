12f25f56ed8fae6232844ed5fd00a7962729b33b
John LaGrone <jlagrone@gmail.com>
2026-05-27 10:12:37 -0500

    add deprecation message

--- modules/m3/applications/apptainer/1.1.6.lua
+++ modules/m3/applications/apptainer/1.1.6.lua
@@ -5,6 +5,9 @@ source_sh('bash', '/hpc/sys/apps/apptainer/current/share/bash-completion/complet
 source_sh('bash', '/hpc/sys/apps/apptainer/current/share/bash-completion/completions/singularity')
 setenv("APPTAINER_BIND","/tmp,/hpc,/lustre/work/client,/lustre/scratch/client,/work,/work/projects:/projects,/cm/local/apps/slurm,/var/lib/sss,/etc/passwd")
 
+LmodMessage("\n\nApptainer 1.1.6 is deprecated. It will be removed from the system June 15, 2026. \nPlease use apptainer/1.5.0 instead. If you experience issues with the new version, please report them. \n")
+
+
 help([[Name: Apptainer
 Version: 1.1.6
 Website: https://apptainer.org/
