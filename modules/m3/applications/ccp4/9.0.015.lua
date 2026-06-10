aba1fa30e0eef2597a3231b042ef9348bd38f324
John LaGrone <jlagrone@gmail.com>
2026-06-10 14:23:29 -0500

    add ccp4 module

---
+++ modules/m3/applications/ccp4/9.0.015.lua
@@ -0,0 +1,10 @@
+whatis("CCP4")
+
+source_sh('bash', '/hpc/m3/apps/ccp4/9.0.015/ccp4-9/bin/ccp4.setup-sh')
+
+help([[Name: CCP4
+Version: 9.0.015
+Website: https://www.ccp4.ac.uk/
+
+CCP4: Software for Macromolecular X-Ray Crystallography
+]])
