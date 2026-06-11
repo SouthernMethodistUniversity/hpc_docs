9790c67aa388e8f606b4c3dba4e3e5ed6c5e2217
John LaGrone <jlagrone@gmail.com>
2026-06-11 14:50:53 -0500

    add mp module

---
+++ modules/mp/apps/miniforge/26.3.2-3.lua
@@ -0,0 +1,13 @@
+whatis("Miniforge")
+family("conda")
+extensions("conda/26.3.2", "mamba/2.5.0")
+
+source_sh('bash', '/hpc/mp/apps/miniforge/26.3.2-3/etc/profile.d/conda.sh')
+source_sh('bash', '/hpc/mp/apps/miniforge/26.3.2-3/etc/profile.d/mamba.sh')
+
+help([[Name: Miniforge
+Version: 26.3.2-3
+Website: https://github.com/conda-forge/miniforge
+
+Miniforge provides minimal installations for Conda and Mamba with conda-forge pre-configured.
+]])
