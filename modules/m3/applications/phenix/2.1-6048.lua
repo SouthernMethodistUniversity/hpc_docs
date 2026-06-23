e6be592bc8d0fe49f9378bdaa36c8cb0d4f37078
John LaGrone <jlagrone@gmail.com>
2026-06-23 10:58:22 -0500

    add phenix

---
+++ modules/m3/applications/phenix/2.1-6048.lua
@@ -0,0 +1,12 @@
+whatis("Phenix)
+family("phenix")
+
+source_sh('bash', '/hpc/m3/apps/phenix/2.1-6048/phenix_env.sh')
+
+help([[Name: Phenix
+Version: 2.1-6048
+Website: https://phenix-online.org/
+License Owners: John LaGrone / requested by Zoltowski / Approved by Dr. Hites
+
+A comprehensive software package for macromolecular structure determination using crystallographic (X-ray, neutron and electron) and electron cryo-microscopy data.
+]])
