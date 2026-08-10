e8477b448f0c686b51d676fef4dfe9d8b4c0d59b
John LaGrone <jlagrone@gmail.com>
2026-08-10 09:36:22 -0500

    add new version of ampl

---
+++ modules/m3/applications/ampl/20260723.lua
@@ -0,0 +1,23 @@
+whatis("AMPL")
+family("ampl")
+conflict("ampl")
+
+local home = os.getenv("HOME")
+local tmp_hostname = os.getenv('HOSTNAME')
+local lic_path = pathJoin(pathJoin(pathJoin(home, '.ampl'), tmp_hostname), 'ampl.lic')
+
+setenv("AMPL_LICFILE", lic_path)
+setenv("AMPLKEY_RUNTIME_DIR", pathJoin(home, '.ampl'))
+
+append_path("PATH", "/hpc/m3/apps/ampl/ampl_20260723")
+
+local activate_cmd  = '/hpc/m3/apps/ampl/ampl_20260723/ampl < /hpc/m3/apps/ampl/ampl_20260723/activate_key'
+execute{cmd=activate_cmd, modeA={"load"}}
+
+help([[Name: AMPL
+Version: ampl_20260723
+Website: https://ampl.com/
+License Owners : Eli Olinick
+
+AMPL is an algebraic modeling language to describe and solve high-complexity problems for large-scale mathematical computing.
+]])
